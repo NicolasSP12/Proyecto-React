e986da586b0b2a34f3d06d4550b0f1b60c30abdd
Melanie <mmantilla930@gmail.com>
2022-06-23 19:59:49 -0500

    home+buscador+detallesunido

--- proyecto-ecommerce/src/components/boton/Button.jsx
+++ proyecto-ecommerce/src/components/boton/Button.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+
 
 export default class Button extends Component {
   render() {
     return (
-      <Link to = '/'>
+      
       <button type="button" class="btn btn-info">Ver más</button>
-      </Link>
-        
     )
   }
 }
--- proyecto-ecommerce/src/components/vistaProducto/VistaProducto.jsx
+++ proyecto-ecommerce/src/components/vistaProducto/VistaProducto.jsx
@@ -5,28 +5,21 @@ const VistaProducto = (props) => {
     const {nombre, imagen, id} = props
     return(
         <>
-        <div className=''> 
+         
             <div className='col' key={id}>
                 <div className='borde'>
                     <img  src={imagen} className="card-img-top" alt='imagenesProductos'/>
                     <h5>
                     {nombre}
                     </h5> 
+                    <Link to={id}>
                     <Button/>
+                    </Link>
                 </div>
             </div>  
-        </div>       
+              
         </>  
     )
 }
 
 export default VistaProducto
-
-{/* <div className="card">
-            <figure>
-                <img src={imagen} alt='imagen'/>
-            </figure>
-        </div>
-        <div className="card-content">
-            <p>{nombre}</p>
-        </div> */}
